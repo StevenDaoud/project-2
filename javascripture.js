2ec6021de218e8f799f223cf3dac2e8777c3bcc0
Cat Litten <catherine@spothero.com>
2019-03-20 19:34:32 -0500

    Beauty. Pure beauty.

---
+++ javascripture.js
@@ -0,0 +1,12 @@
+var addingCats= function(catUno , catDos) {
+    return catUno +                catDos;
+  };
+  
+  var MULTIPLYING_CATS = function(catUn, CatDeux) {
+    return catUn * CatDeux;
+  };
+  
+  var MeowMeowmeow = function( meow_meow, meow_meow) {
+    return mew_mew
+  }
+  
